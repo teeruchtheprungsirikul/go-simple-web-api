d834135bddc913eb24b7e1a23469edb26e4f9130
teeruchtheprungsirikul <teeruch_th@hotmail.com>
2025-07-30 02:50:02 +0700

    Start Web Server

---
+++ cmd/api/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// สร้างตัวแปรกำหนด port ที่ใช้
+const port = 8080
+
+// สร้าง application struct สำหรับเก็บ config และ database connection
+type application struct {
+	Domain string
+}
+
+func main() {
+
+	// Set application config
+	var app application
+	app.Domain = "example.com"
+
+	// Read from command line arguments
+
+	// Connect to database
+
+	// Start the server
+	fmt.Println("Starting server on ", app.Domain)
+	log.Printf("Starting server on port %d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
