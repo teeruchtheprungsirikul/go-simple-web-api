9dd771329c3128e3c6a68b3c71727c20dbc2e9d2
teeruchtheprungsirikul <teeruch_th@hotmail.com>
2025-07-30 18:41:59 +0700

    Connecting to PostgreSQL

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,7 +13,9 @@ const port = 8080
 
 // สร้าง application struct สำหรับเก็บ config และ database connection
 type application struct {
+	DSN    string
 	Domain string
+	DB     *sql.DB
 }
 
 func main() {
@@ -21,8 +25,20 @@ func main() {
 	app.Domain = "example.com"
 
 	// Read from command line arguments
-
+	flag.StringVar(&app.DSN, "dsn",
+		"host=localhost port=5432 dbname=gosampledb user=postgres password=030845 sslmode=disable timezone=UTC connect_timeout=5",
+		"Postgres connection string")
+	flag.Parse()
 	// Connect to database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
+	// Close the database connection
+	defer app.DB.Close()
 
 	// http.HandleFunc("/", Hello)
 	// http.HandleFunc("/about", About)
@@ -30,7 +46,7 @@ func main() {
 	// Start the server
 	fmt.Println("Starting server on ", app.Domain)
 	log.Printf("Starting server on port %d", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
